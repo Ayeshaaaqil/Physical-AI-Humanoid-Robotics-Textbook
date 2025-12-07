0ded7cfd2fe3d8814c4e8b71d3140f6fdc369389
Ayesha <163387252+Ayeshaaaqil@users.noreply.github.com>
2025-12-07 13:27:59 +0500

    Add files via upload

---
+++ my-website/src/theme/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatKitWidget from '../../src/components/ChatKitWidget';
+
+function Root({ children }) {
+  return (
+    <>
+      {children}
+      <ChatKitWidget />
+    </>
+  );
+}
+
+export default Root;
